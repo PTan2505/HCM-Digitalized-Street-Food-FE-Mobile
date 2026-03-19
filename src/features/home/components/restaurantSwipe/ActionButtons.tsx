fa0bf6e249d17c6b9e8f6d967f09c0b3444d9071
Phuc Tan <163091128+PTan2505@users.noreply.github.com>
2026-03-19 23:57:31 +0700

    feat: add menu and review icons to ActionButtons component

--- src/features/home/components/restaurantSwipe/ActionButtons.tsx
+++ src/features/home/components/restaurantSwipe/ActionButtons.tsx
@@ -1,3 +1,6 @@
+import menuIcon from '@assets/icons/menu.svg';
+import reviewIcon from '@assets/icons/review.svg';
+import SvgIcon from '@components/SvgIcon';
 import type { ActiveBranch } from '@features/home/types/branch';
 import { useNavigation } from '@react-navigation/native';
 import type { JSX } from 'react';
@@ -25,6 +28,7 @@ const ActionButtons = ({
   return (
     <View className="flex-row items-center justify-center gap-2 px-4 pb-4">
       <TouchableOpacity
+        className="h-[28px] w-[49px] items-center justify-center rounded-[36px] bg-[rgba(237,237,237,1)] p-[5px] pb-[7px]"
         onPress={() => {
           onBookmark?.();
           navigation.navigate('RestaurantDetails', {
@@ -34,7 +38,44 @@ const ActionButtons = ({
             onRatingUpdate,
           });
         }}
-        className="h-14 flex-1 items-center justify-center rounded-full bg-[#FF6B2C]"
+      >
+        <SvgIcon
+          icon={menuIcon}
+          width={'17px'}
+          height={'12px'}
+          color="#FF6B2C"
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        className="h-[28px] w-[49px] items-center justify-center rounded-[36px] bg-[rgba(237,237,237,1)] p-[5px] pb-[7px]"
+        onPress={() => {
+          onBookmark?.();
+          navigation.navigate('RestaurantDetails', {
+            branch,
+            displayName,
+            tab: 'reviews',
+            onRatingUpdate,
+          });
+        }}
+      >
+        <SvgIcon
+          icon={reviewIcon}
+          width={'14px'}
+          height={'14px'}
+          color="#FF6B2C"
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          onBookmark?.();
+          navigation.navigate('RestaurantDetails', {
+            branch,
+            displayName,
+            tab: 'menu',
+            onRatingUpdate,
+          });
+        }}
+        className="h-[42px] flex-1 items-center justify-center rounded-full bg-[#FF6B2C]"
       >
         <Text className="text-base font-bold text-white">
           {t('actions.view_detail')}
